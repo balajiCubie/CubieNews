6ebcf0436f06b56d64186cbdb86930a3027dfb7f
Balaji K <balajibala76394@gmail.com>
2023-06-20 22:54:33 +0530

    Configure a

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ const nextConfig = {
   // basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH : "",
   output: "export",
   // basePath: "/CubieNews",
-  // images: {
-  //   loader: 'akamai',
-  //   path: process.env.NEXT_PUBLIC_URL,
-  // },  
+  images: {
+    loader: 'akamai',
+    path: process.env.NEXT_PUBLIC_URL,
+  },  
   // images: {
   //   unoptimized: true,
   // },
