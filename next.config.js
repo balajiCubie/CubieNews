f34b3a03e916f83bc5558127dbb01104ef10b68f
Balaji K <balajibala76394@gmail.com>
2023-06-20 22:59:37 +0530

    just1

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
-  // basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH : "",
+  basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH : "",
   output: "export",
-  // basePath: "/CubieNews",
+  basePath: "/CubieNews",
   images: {
     loader: 'akamai',
     path: process.env.NEXT_PUBLIC_URL,
